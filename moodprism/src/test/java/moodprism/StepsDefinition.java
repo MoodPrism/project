2c7023e0a0f3896ba2acf9b2b9bf0c50f6ab289e
mapinheiro <marianaapinheiro@ua.pt>
2020-05-06 23:35:28 +0100

    add pom e test cucumber

---
+++ moodprism/src/test/java/moodprism/StepsDefinition.java
@@ -0,0 +1,12 @@
+package moodprism;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+public class StepsDefinition {
+   
+
+}
