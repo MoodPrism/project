8f7f4a9a987b6336740838f33e291b9bda6e526d
mapinheiro <marianaapinheiro@ua.pt>
2020-05-09 16:08:52 +0100

    add stepsDefinition

--- moodprism/src/test/java/moodprism/StepsDefinition.java
+++ moodprism/src/test/java/moodprism/StepsDefinition.java
@@ -1,8 +1,48 @@
 package moodprism;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 
 
 public class StepsDefinition {
    
+    @Given("^I have the application running$")
+    public void i_have_the_application_running() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I press Escape constantly$")
+    public void i_press_Escape_constantly() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the page refreshes$")
+    public void the_page_refreshes() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the result on the screen should indicate frustration$")
+    public void the_result_on_the_screen_should_indicate_frustration() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I write as I usually do$")
+    public void i_write_as_I_usually_do() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the result on the screen should indicate normal state of mind$")
+    public void the_result_on_the_screen_should_indicate_normal_state_of_mind() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 
 }
